3fc4aa05885c4435d498cd585f96da23d935361e
mhdi_nzari <mahdi.nazari.gb008@gmail.com>
2023-07-17 18:57:42 +0430

    mend

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -2,11 +2,20 @@ import "./global.css";
 
 const word = "text";
 const HangmanWord = () => {
+  const guessedLetters = ["t", "e", "g"];
   return (
     <div className="hangWord_wrapper">
       {word.split("").map((letter, index) => (
-        <span style={{ borderBottom: ".1em solid black" }}>
-          <span style={{ visibility: "hidden" }}>{letter}</span>
+        <span style={{ borderBottom: ".1em solid black" }} key={index}>
+          <span
+            style={{
+              visibility: guessedLetters.includes(letter)
+                ? "visible"
+                : "hidden",
+            }}
+          >
+            {letter}
+          </span>
         </span>
       ))}
     </div>
